4c47d50f72e63980f354631358c46f094406638f
Kaan Kızılağıl <kaankizilagil@gmail.com>
2019-06-26 21:15:16 +0300

    Add files via upload
    It is a desktop application developed with Swing interface. After login to the admin panel, you can add employees to the system, update and delete existing employees.

---
+++ Employee Project/src/Database.java
@@ -0,0 +1,9 @@
+
+public class Database {
+    
+    public static final String host = "localhost";
+    public static final int port = 8889;
+    public static final String db_name = "Engineering_Project_2";
+    public static final String kullanici_adi = "root";
+    public static final String parola = "root";
+}
